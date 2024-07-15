6b29b6dc82f00577f45c1740c9bfa785fe3ff6be
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-07-15 15:01:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from transformers import pipeline
 import os
-from langchain.llms import OpenAI  # Correct import for OpenAI from Langchain
+from langchain.llms import OpenAI
 
 # Initialize emotion detection pipeline
 emotion_detector = pipeline('sentiment-analysis', model='bhadresh-savani/distilbert-base-uncased-emotion')
@@ -19,10 +19,10 @@ def get_opposite_emotion(emotion):
     return opposite_emotions.get(emotion, 'neutral')
 
 # Function to generate story using Langchain
-def generate_story(opposite_emotion, api_key):
+def generate_story(opposite_emotion, api_key, user_name):
     os.environ["OPENAI_API_KEY"] = api_key
     llm = OpenAI(model_name="gpt-3.5-turbo-instruct")
-    our_query = f"Write a story that evokes {opposite_emotion}."
+    our_query = f"Write a story that evokes {opposite_emotion} in which the main character's name is {user_name}."
     story = llm(our_query)
     return story
 
@@ -36,7 +36,11 @@ def main():
         st.warning("Please enter your OpenAI API key.")
         return
     
-    user_input = st.text_input("How are you feeling today?")
+    user_name = st.text_input("Enter your name:")
+    user_input = None
+    
+    if user_name:
+        user_input = st.text_input(f"{user_name}, How are you feeling today?")
     
     if user_input:
         emotions = emotion_detector(user_input)
@@ -46,7 +50,7 @@ def main():
         st.write(f"Detected Emotion: {primary_emotion}")
         st.write(f"Opposite Emotion: {opposite_emotion}")
         
-        story = generate_story(opposite_emotion, api_key)
+        story = generate_story(opposite_emotion, api_key, user_name)
         st.write("Here's a story for you:")
         st.write(story)
 
